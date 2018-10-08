ff08c31d268c1680283e603fae4db5127ecde66b
huangwenzi <35164459+huangwenzi@users.noreply.github.com>
2018-10-08 09:41:19 +0800

    huangwen
    1.添加截图功能
    2.鼠标功能代码优化
    3.添加简单使用说明

--- keysEvent.py
+++ keysEvent.py
@@ -1,24 +1,33 @@
 from pynput.mouse import Button, Controller
 mouse = Controller()
+import pyautogui
+import time
 
 # 一些全局定义
 # 辅助键，标识需要特殊处理的事件
 AUXILIARY_KEY = "0"
 
+# 这里返回值的枚举
+class KeysRet():
+    isFun = 0
+    noFun = 1
+    isExit = 2
+
 # 这里处理键盘按下的事件
-class KeysEvent:
+class KeysEvent():
 
     def __init__(self):
         pass
-        
+
 
     # 按下键
     # data : 用户数据
     # key : 传入的key对象
     def keyDown(self, data, key):
         # 如果是功能键，进入功能键函数，退出普通按键效果
-        if self.isFun(data, key):
-            return
+        ret = self.isFun(data, key)
+        if ret != keysRet.noFun:
+            return ret
 
         # 提取按下的键
         # 鉴于pynput只能在key.char中保存一般按键，不能保存特殊功能键
@@ -26,14 +35,14 @@ class KeysEvent:
             # 把按下的键值存入数据中
             if key.char not in data.keyDown:
                 data.keyDown.append(key.char)
-                print(data.keyDown)
+                # print(data.keyDown)
         # 特殊键先不处理
         else :
-            return
+            return ret
 
         # 如果快捷键功能没有开启，则退出
         if not data.openFlag:
-            return
+            return ret
 
         # 执行快捷键操作
         char = key.char
@@ -46,6 +55,8 @@ class KeysEvent:
                 mouse.click(Button.left)
                 # print("move,X:" + str(postion[0]) + "Y:" + str(postion[1]) )
 
+        return ret
+
 
     # 放开键
     # data : 用户数据
@@ -55,17 +66,17 @@ class KeysEvent:
             # 把按下的键值存入数据中
             if key.char in data.keyDown:
                 data.keyDown.remove(key.char)
-                print(data.keyDown)
+                # print(data.keyDown)
 
 
     # 检查是不是功能键
     # data : 用户数据
     # key : 传入的key对象
-    # return : 返回是否特殊功能的bool值
+    # return : 返回 0:不是功能处理  1:是功能处理  2:退出
     def isFun(self, data, key):
         # 如果辅助键没有同时按下，则不是功能处理
         if AUXILIARY_KEY not in data.keyDown:
-            return False
+            return keysRet.noFun
 
         # 下面是各功能键的处理，不包含特殊键
         if hasattr(key, "char"):
@@ -78,12 +89,21 @@ class KeysEvent:
             elif char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
                 data.postion[char] = mouse.position
                 print(data.postion)
+            # 截图
+            elif char == "+":
+                # 用当前时间做名字保存
+                name = time.strftime("screenshot\%Y-%m-%d_%H-%M-%S.png", time.localtime())
+                pyautogui.screenshot(name)
+            # 退出
+            elif char == "-":
+                return keysRet.isExit
             # 不存在的功能键就返回false
             else :
-                return False
+                return keysRet.noFun
 
-        return True
+        return keysRet.isFun
 
 
 # 实例化这个过程处理类
 keysEvent = KeysEvent()
+keysRet = KeysRet()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pynput import keyboard
 import keysEvent as keysEventMd
 keysEvent = keysEventMd.keysEvent
+keysRet = keysEventMd.keysRet
 import dataMgr as dataMgrMd
 
 # 数据管理的类
@@ -8,7 +9,10 @@ dataMgr = dataMgrMd.DataMgr()
 
 # 键盘按下
 def on_press(key):
-    keysEvent.keyDown(dataMgr, key)
+    ret = keysEvent.keyDown(dataMgr, key)
+    # 返回的是退出
+    if ret == keysRet.isExit:
+        return False
 
 # 键盘弹起
 def on_release(key):
@@ -20,4 +24,9 @@ with keyboard.Listener(
         on_release=on_release) as listener:
     listener.join()
 
-
+# 使用方法
+# 0是目前唯一辅助键
+# 0 + "1~9" 可以记录鼠标位置给对应数字
+# "1~9"     触发鼠标移动到对应位置并点击功能
+# 0 + "."   开启和关闭功能
+# 0 + "+"   截取全屏，保存在当前目录下的screenshot
